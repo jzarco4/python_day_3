dad9bb851854139d873b0459d1def5658b101b82
Zarco <jzarco4@cps.k12.il.us>
2024-08-30 15:06:28 -0500

    first commit

---
+++ lesson.py
@@ -0,0 +1,60 @@
+#create a print function that prints
+#out the string "hello world"
+print("Hello World")
+#ask the user for the day of the week
+# day_of_week = input("what day of the week is it?")
+# print("Today is"+ day_of_week)
+#concatenation is when you add two strings together
+#using a plus sign +
+# movies_this_week = input("what movies are you watching this week?")
+# print("I am watching"+movies_this_week+"this week")
+# mood = input("How are you feeling today")
+# print("i am feeling" + mood )
+# data types for variables in python
+# strings - text
+name = "john"# this is a string data type
+#whenever it is wrapped in quotes it is
+year ="2024" 
+# integers - whole numbers
+year = 2024 # this is an intiger data type
+#does not need to be wrapped in quotes
+YearFourFromNow = 2028
+subtract = YearFourFromNow - year
+# floats - decimal numbers
+priceBigMac = 3.99 #thgis is a float data type
+priceDoublePounder = 4.99
+totalPrice = priceBigMac + priceDoublePounder
+print(totalPrice)
+#booleans - true or false
+isRaining = False # this is a boolean data type
+print(isRaining)
+# lists - a collection of items
+groceries = ("apples", "bananas", "carrots")
+print(groceries)
+#challenge 1
+#you and your family are goping to a movie and
+#dinner. you need a list of movies to choose from
+# address of the restaurant 
+#the place of the restaurant
+#the time of dinner
+#the time of the movie
+#the name of the movie
+print("we are going to see" + MovieName)
+"at" + movieTime +
+"and then eat dinner at"
+#the price of the movie
+moviePrice = 12.99
+#the price of dinner
+dinnerPrice= 15.90
+#playing to see if movie is playing in the evening
+#how many people are going
+#print the entire output in one sentence
+name_of_restaurant = input ("what restaurant are you going too?")
+print("I am going to" + name_of_restaurant)
+time_of_movie = input ("what time is the movie?")
+print("i am going at" + time_of_movie)
+time_of_dinner = input ("what time is dinner?")
+print("Dinner starts at" + time_of_dinner)
+MovieName = input ("What movie are you watching?")
+print("")
+moviePrice =
